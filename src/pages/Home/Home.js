edc9fcf1aa741031d71d3d189c8053de657082a8
necalero <ne.calero@uniandes.edu.co>
2022-12-12 20:14:58 -0500

    Added functionality

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -4,7 +4,8 @@ import "./Home.css";
 export const Home = () => {
   const [data, setData] = useState({
     monthly: "10",
-    year: "1990",
+    startYear: "1990",
+    endYear: new Date().getFullYear(),
     rate: "5",
   });
 
@@ -47,7 +48,7 @@ export const Home = () => {
   const handleInputChange = (event) => {
     let checks = true;
     if (
-      event.target.name === "year" &&
+      event.target.name === "startYear" &&
       (event.target.value < 1990 ||
         event.target.value > new Date().getFullYear())
     ) {
@@ -65,31 +66,31 @@ export const Home = () => {
     }
   };
 
-  const getInvestment = (monthly, year, rate) => {
+  const getInvestment = (monthly, startYear, endYear, rate) => {
     let result = 0;
-    if (year >= 1990) {
-      let yearly = monthly * 12;
+    let yearly = monthly * 12;
       result = yearly;
       let increment = 1 + rate / 100;
-      let actualYear = new Date().getFullYear();
-      for (let i = year; i < actualYear; i++) {
+      for (let i = startYear; i < endYear; i++) {
         result = result * increment + yearly;
       }
       result = result.toFixed(2);
-    }
     return result;
   };
 
-  const getSP = (monthly, year) => {
+  const getSP = (monthly, startYear, endYear) => {
     let result = 0;
-    if (year < 1990) {
-      year = 1990;
+    let actualYear = new Date().getFullYear();
+
+    if(endYear>actualYear)
+    {
+        endYear = actualYear
     }
+    
     let yearly = monthly * 12;
     result = yearly;
     let increment = 0;
-    let actualYear = new Date().getFullYear();
-    for (let i = year; i < actualYear; i++) {
+    for (let i = startYear; i < endYear; i++) {
       increment = 1 + sp500[i] / 100;
       result = result * increment + yearly;
     }
@@ -98,15 +99,19 @@ export const Home = () => {
     return result;
   };
 
+  function numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  }
+
   return (
     <>
       <div className="centered-main">
         <h1>Ready to start investing?</h1>
         <h4>Run a custom scenario now:</h4>
         <form className="calc-inputs">
-          <div className="row">
-            <div className="col-md-5">
-              <div className="input-group mb-2">
+          <div className="row justify-content-center">
+            <div className="col-md-4 ">
+              <div className="input-group mb-2 input-field">
                 <div className="input-group-prepend">
                   <div className="input-group-text">$</div>
                 </div>
@@ -122,8 +127,8 @@ export const Home = () => {
               </div>
             </div>
 
-            <div className="col-md-3">
-              <div className="input-group mb-2">
+            <div className="col-md-2 ">
+              <div className="input-group mb-2 input-field">
                 <div className="input-group-prepend">
                   <div className="input-group-text">%</div>
                 </div>
@@ -139,14 +144,25 @@ export const Home = () => {
               </div>
             </div>
 
-            <div className="col-md-3">
+            <div className="col-md-2">
               <input
                 type="number"
-                className="form-control"
+                className="form-control input-field"
                 placeholder="Year started"
                 min="1990"
                 max="2022"
-                name="year"
+                name="startYear"
+                onChange={handleInputChange}
+              />
+            </div>
+
+            <div className="col-md-2">
+              <input
+                type="number"
+                className="form-control input-field"
+                placeholder="End year"
+                min="1990"
+                name="endYear"
                 onChange={handleInputChange}
               />
             </div>
@@ -156,31 +172,38 @@ export const Home = () => {
 
       <div className="results-section">
         <div className="flex-container">
-          <div className="box-one">
-            <div className="big-number">${getInvestment(data.monthly, data.year, data.rate)}</div>
+          <div className="result-box">
+            <div className="big-number">
+              ${numberWithCommas(getInvestment(data.monthly, data.startYear, data.endYear, data.rate))}
+            </div>
+            <div className="box-title">Custom scenario</div>
             <div className="results">
               {" "}
               Had you started investing ${data.monthly} every month since{" "}
-              {data.year} with {data.rate}% interest rate, you would have $
-              {getInvestment(data.monthly, data.year, data.rate)} by now.
+              {data.startYear} with {data.rate}% interest rate, you would have $
+              {numberWithCommas(getInvestment(data.monthly, data.startYear, data.endYear, data.rate))} by {data.endYear}.
             </div>
           </div>
-          <div className="box-two">
-            <div className="big-number">${getSP(data.monthly, data.year)}</div>
+          <div className="result-box">
+            <div className="big-number">${numberWithCommas(getSP(data.monthly, data.startYear, data.endYear))}</div>
+            <div className="box-title">S&P500 scenario</div>
             <div className="results">
               {" "}
               Had you started investing ${data.monthly} every month since{" "}
-              {data.year} in S&P500, you would have $
-              {getSP(data.monthly, data.year)} by now.
+              {data.startYear} in S&P500, you would have $
+              {numberWithCommas(getSP(data.monthly, data.startYear, data.endYear))} by {data.endYear}.
             </div>
           </div>
-          <div className="box-three">
-            <div className="big-number">${data.monthly * 12 * (2023 - data.year)}</div>
+          <div className="result-box">
+            <div className="big-number">
+              ${numberWithCommas(data.monthly * 12 * (data.endYear - data.startYear))}
+            </div>
+            <div className="box-title">Savings scenario</div>
             <div className="results">
               {" "}
               Had you started saving up ${data.monthly} every month since{" "}
-              {data.year}, you would have $
-              {data.monthly * 12 * (2023 - data.year)} by now.
+              {data.startYear}, you would have $
+              {numberWithCommas(data.monthly * 12 * (data.endYear - data.startYear))} by {data.endYear}.
             </div>
           </div>
         </div>
